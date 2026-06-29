08a59d80ada2fc6184eca8b5011289785bb6ba50
mame77 <morinaga.mqme@gmail.com>
2026-06-30 01:47:30 +0900

    bashを更新

--- modules/env.nix
+++ modules/env.nix
@@ -20,10 +20,19 @@
   # bash
   programs.bash = {
     enable = true;
+    # nix-config の PS1 を NixOS デフォルトで上書きしない
+    promptInit = "";
     interactiveShellInit =
       ''
         eval "$(${pkgs.direnv}/bin/direnv hook bash)"
       ''
       + builtins.readFile ../dotfiles/bashrc/bashrc;
   };
+
+  # home-manager 時代の ~/.bashrc が PS1 を \w で上書きするのを防ぐ
+  system.activationScripts.removeStaleHomeManagerBashrc.text = ''
+    if [ -L /home/mame/.bashrc ] && readlink /home/mame/.bashrc | grep -q home-manager; then
+      rm /home/mame/.bashrc
+    fi
+  '';
 }
