0d54a7f5f864b419202c67ba9e1d2d2d2db4f6bb
mame77 <morinaga.mqme@gmail.com>
2026-07-06 00:21:22 +0900

    niri

---
+++ modules/fcitx5.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+
+{
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+        fcitx5-qt6
+      ];
+      waylandFrontend = true;
+    };
+  };
+
+  services.xserver.desktopManager.runXdgAutostartIfNone = true;
+}
--- modules/noctalia.nix
+++ modules/noctalia.nix
@@ -9,21 +9,19 @@
     programs.noctalia = {
       enable = true;
       systemd.enable = true;
-
-      settings = {
-        theme = {
-          mode = "dark";
-          source = "builtin";
-          builtin = "Catppuccin";
-        };
-        wallpaper = {
-          enabled = true;
-          "default.path" = "/home/mame/Pictures/Wallpapers/default.png";
-        };
-      };
     };
 
     home.file."Pictures/Wallpapers/default.png".source =
       ../dotfiles/wallpapers/default.png;
+
+    # Symlink the TOML config to the dotfile in the repo for two-way sync.
+    # We can't use programs.noctalia.settings because Nix would copy the
+    # git-tracked file into /nix/store (read-only), breaking GUI writes.
+    home.activation.linkNoctaliaConfig = ''
+      mkdir -p "$HOME/.config/noctalia"
+      rm -f "$HOME/.config/noctalia/config.toml"
+      ln -s /home/mame/nix-config/dotfiles/noctalia/config.toml \
+        "$HOME/.config/noctalia/config.toml"
+    '';
   };
 }
